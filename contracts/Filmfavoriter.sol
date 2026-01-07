1d66f9fb7f826958bbf8359d22e195a4e9337f5f
forselius98 <elfor_98@outlook.com>
2026-01-07 11:37:59 +0100

    punkter

--- contracts/Filmfavoriter.sol
+++ contracts/Filmfavoriter.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.31;
 
+//Kontraktet ska innehåll följande element:
+// Minst en struct eller enum
+// Minst en mapping eller array
+// En constructor
+// Minst en custom modifier
+// Minst ett event för att logga viktiga händelser
+// Minst ett custom error
+// Minst en require, en assert, och en revert
+// En fallback och/eller receive funktion
+
 contract filmFavoriter {
   
   struct Omrostning {
@@ -12,4 +22,6 @@ contract filmFavoriter {
     mapping(string => uint8) rostAntal;
     mapping(address => bool) harRostat;
   }
+  //-state variabel--
+
 }
