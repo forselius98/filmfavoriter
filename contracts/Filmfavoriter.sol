37fa1943fcb64f435e28c6ffe40a7e0b31d7427a
Hani Baloch <hanisheksson84@gmail.com>
2026-01-07 18:46:41 +0100

    Update Filmfavoriter.sol

--- contracts/Filmfavoriter.sol
+++ contracts/Filmfavoriter.sol
@@ -48,3 +48,35 @@ contract filmFavoriter {
         require(block.timestamp < omrostning.slutTid, "Tiden har gatt ut");
     }
 }
+
+
+
+
+
+
+Hani- påbörjat första delen
+
+pragma solidity 0.8.31;
+
+contract FilmFavoriter {
+    address public owner;  
+    
+    struct Vote {
+        string titel;
+        uint vote; 
+        address delegate;
+    }
+    
+    Vote[5] public votes;
+    uint public voteCount = 0;  
+    mapping(address => Vote[5]) public polls;  
+    
+    constructor() {  
+        owner = msg.sender;
+    }
+    
+    function createPoll() public {  
+        
+    }
+}
+}
